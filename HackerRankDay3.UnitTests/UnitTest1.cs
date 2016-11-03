48e9fe52333a131fc9d88aa24e31c7854fb09675
Adrian Farnell <adrianjfarnell@gmail.com>
2016-11-03 00:16:33 +0000

    initial commit

---
+++ HackerRankDay3.UnitTests/UnitTest1.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HackerRankDay3;
+
+namespace HackerRankDay3.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IfNIsOneItsWeird()
+        {
+            //arrange 
+            int n = 1;
+
+            //act
+            string result = Work.IsItWeird(n);
+
+            //assert
+            Assert.AreEqual(result, "Weird");
+
+        }
+
+        [TestMethod]
+        public void IfNIsFiveItsWeird()
+        {
+            //arrange 
+            int n = 5;
+
+            //act
+            string result = Work.IsItWeird(n);
+
+            //assert
+            Assert.AreEqual(result, "Weird");
+
+        }
+
+        [TestMethod]
+        public void IfNIsTwoItsNotWeird()
+        {
+            //arrange 
+            int n = 2;
+
+            //act
+            string result = Work.IsItWeird(n);
+
+            //assert
+            Assert.AreEqual(result, "Not Weird");
+
+        }
+
+        [TestMethod]
+        public void IfNIsFourItsNotWeird()
+        {
+            //arrange 
+            int n = 4;
+
+            //act
+            string result = Work.IsItWeird(n);
+
+            //assert
+            Assert.AreEqual(result, "Not Weird");
+
+        }
+
+        [TestMethod]
+        public void IfNIsSixItsWeird()
+        {
+            //arrange 
+            int n = 6;
+
+            //act
+            string result = Work.IsItWeird(n);
+
+            //assert
+            Assert.AreEqual(result, "Weird");
+
+        }
+
+        [TestMethod]
+        public void IfNIsTwentyItsNotWeird()
+        {
+            //arrange 
+            int n = 20;
+
+            //act
+            string result = Work.IsItWeird(n);
+
+            //assert
+            Assert.AreEqual(result, "Not Weird");
+
+        }
+
+        [TestMethod]
+        public void IfNIsTwentyTwoItsNotWeird()
+        {
+            //arrange 
+            int n = 22;
+
+            //act
+            string result = Work.IsItWeird(n);
+
+            //assert
+            Assert.AreEqual(result, "Not Weird");
+
+        }
+
+        [TestMethod]
+        public void IfNIsEighteenItsWeird()
+        {
+            //arrange 
+            int n = 18;
+
+            //act
+            string result = Work.IsItWeird(n);
+
+            //assert
+            Assert.AreEqual(result, "Weird");
+
+        }
+    }
+}
---
+++ HackerRankDay3/Work.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankDay3
+{
+    public class Work
+    {
+        public static string IsItWeird(int n)
+        {
+            if (n % 2 != 0)
+            {
+                return "Weird";
+            }
+            else
+            {
+                if (n > 5 && n < 20)
+                {
+                    return "Weird";
+                }
+
+                return "Not Weird";
+            }
+            //throw new NotImplementedException();
+        }
+    }
+}
